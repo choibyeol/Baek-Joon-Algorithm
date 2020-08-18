18e2f965cc06bd058f3036bb44e6e5393390a6ee
Choi Byeol <honey333888@naver.com>
2020-08-18 17:59:46 +0900

    분수 찾기

--- step9/1193.py
+++ step9/1193.py
@@ -1,11 +1,14 @@
 X = int(input())
-stage, key_X = 1, 1
-while key_X + stage <= X:
-    key_X += stage
+stage = 1
+while X > stage:
+    X -= stage
     stage += 1
-step = X - key_X
-x, y = step + 1, stage - step
-if stage % 2 == 0:
-    print('{}/{}'.format(x, y))
+
+if(stage%2 == 1):
+    x = stage+1 - X
+    y = stage+1 - x
+    print("{}/{}".format(x, y))
 else:
-    print('{}/{}'.format(y, x))
+    x = stage+1 - X
+    y = stage+1 - x
+    print("{}/{}".format(y, x))
