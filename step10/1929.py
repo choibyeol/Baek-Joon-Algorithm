178a3be881f91a92222e99f43b656a78174144d2
Choi Byeol <honey333888@naver.com>
2020-08-23 00:06:00 +0900

    소수 구하기

--- step10/1929.py
+++ step10/1929.py
@@ -1,15 +1,16 @@
-def findN(N):
-    divisor = 1
-    pnum = 0
-    for j in range(N):
-        if(N % divisor == 0):
-            pnum += 1
-        divisor += 1
-    if(pnum == 2):
-        return 1
+import math
+
+def isPrime(N):
+    if N == 1:
+        return False
+
+    n = int(math.sqrt(N)) #제곱근까지만 구해보면 된다. 아니면 시간초과
+    for i in range(2, n+1):
+        if N % i == 0:
+            return False
+    return True
 
 M, N = map(int, input().split())
 for i in range(M, N+1):
-    if(findN(i) == 1):
+    if isPrime(i):
         print(i)
-
